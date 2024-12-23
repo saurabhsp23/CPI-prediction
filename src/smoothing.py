d3820b8bbac43727aeed7a78bc7702377c821efa
saurabhsp23 <144515182+saurabhsp23@users.noreply.github.com>
2024-12-23 10:19:22 -0500

    Update smoothing.py

--- src/smoothing.py
+++ src/smoothing.py
@@ -1,21 +1,3 @@
-# This is a collection of smoothing methods for the AutoML Pipeline 
-
-# GOALS: 1)  Expand this class by surveying different time series smoothing ideas and 
-#            implementing them here with a common API; these include: 
-# 
-#            Survey this smoother models and expand them into the below  
-#            https://www.statsmodels.org/0.9.0/tsa.html
-# 
-#         2) Write assertion test in __init__ to insure that data is of the correct type  
-#            add in typechecking as well
-#
-#         3) Write tests on the output, make sure that it is always the same format, 
-#            and also add interpolation/nan-fillers 
-#        
-#         4) Eventually come back and write integration tests to this and the broader
-#            module
-# 
-#         5) Typing ... do this statically typed 
 
 from constants import TRAINING_DEFAULT_LOOKBACK_WINDOW, LOWESS_FRAC_CONST 
 
@@ -26,6 +8,7 @@ class Smoother:
     ''' 
     Smoother class to work on pre-processing data 
     ''' 
+    
     def __init__(self,data,smoother_type = 'moving_average',lookback_window=21,**kwargs): 
         ''' 
         Initial smoothing methods that we 
@@ -33,10 +16,11 @@ class Smoother:
         Here kwargs are other specific smoother techniques that 
         are required 
         '''
+        
         self.data = data 
         self.smoother_type = smoother_type 
         self.lookback_window = lookback_window
-        self.params_dict = kwargs  ## CHECK THIS! 
+        self.params_dict = kwargs 
 
     def exponential_smoothing(self, col, seasonal_periods=TRAINING_DEFAULT_LOOKBACK_WINDOW, is_seasonal=None, is_trend=None):
         '''
@@ -44,6 +28,7 @@ class Smoother:
         or seasonality fit based on parameters "is_seasonal" and "is_trend". For seasonality, can input a 
         seasonality period (with default being 12). Can choose the start and end point for the prediction values.
         '''
+        
         if is_seasonal:
             is_seasonal = 'add'
         if is_trend:
@@ -58,6 +43,7 @@ class Smoother:
         Takes in a dataframe column and returns an lowess smoothed column. Fraction parameter set to a
         default value of 0.1.
         '''
+        
         smoothed = lowess(col, range(len(col)), frac)
 
         return smoothed[:, 1]
@@ -66,6 +52,7 @@ class Smoother:
         '''
         Takes in a dataframe and returns the moving average based on the window size.
         '''
+        
         moving_avg = data.rolling(window=window).mean().dropna()
 
         return moving_avg
